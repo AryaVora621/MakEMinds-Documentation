d9dc58eb2b8c6a5afe72da33b5e6eb8ccaf3f6b7
AryaVora621 <aryavora621@gmail.com>
2025-02-27 17:44:43 -0500

    Add Vercel Speed Insights

--- src/theme/Layout.js
+++ src/theme/Layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "@theme-original/Layout";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function CustomLayout(props) {
   return (
     <>
       <Layout {...props} />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
